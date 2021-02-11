4c829c2c181f967387776fb365777ffd7d55b5d0
Aashir Shaikh <hi@aashir.me>
2021-02-11 05:23:12 +0500

    pilot

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
+import Spinner from "react-bootstrap/Spinner";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./Footer";
 
 function App() {
+  const [image, setImage] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = () => {
+    setLoading(true);
+    axios
+      .get(
+        `https://api.generated.photos/api/v1/faces?api_key=${process.env.REACT_APP_API_KEY}&order_by=random`
+      )
+      .then((res) => {
+        const uri = res.data.faces[0].urls[4][512];
+        uri && setImage(uri);
+      })
+      .then(() => {
+        setLoading(false);
+      })
+      .catch((err) => console.log(err.message));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__container">
+        <p>Generate Photo with AI</p>
+        {loading && (
+          <div className="loading">
+            <Spinner animation="border" role="status" variant="light"></Spinner>
+          </div>
+        )}
+        {!loading && (
+          <img
+            src={
+              image
+                ? image
+                : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqhrM2eJV7EOIWBlk0nCGIOAvUJGBi7_4kaA&usqp=CAU"
+            }
+            alt="AI IMAGE"
+          />
+        )}
+        <button onClick={handleChange}>GENERATE</button>
+      </div>
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Footer.css";
+import { FaHeart } from "react-icons/fa";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer__content">
+        <p>
+          made with <FaHeart class="heart_icon" /> by{" "}
+          <b class="footer__link">
+            <a href="https://github.com/aaashir">aaashir</a>
+          </b>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
